876b4fa9c217c2b7968c9c7d9634e2800a8627e1
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2018-09-21 00:49:54 +0300

    Implemented ReservationsController

--- BookingApi/Controllers.fs
+++ BookingApi/Controllers.fs
@@ -1,4 +1,14 @@
-﻿namespace BookingApi
+﻿namespace Ploeh.Samples.Booking.HttpApi
 
-type Class1() = 
-    member this.X = "F#"
+open System.Net
+open System.Net.Http
+open System.Web.Http
+
+type HomeController() = 
+    inherit ApiController()
+    member _this.Get() = new HttpResponseMessage()
+
+type ReservationsController() = 
+    inherit ApiController()
+    member _this.Post() (rendition : MakeReservationRendition) = 
+        new HttpResponseMessage(HttpStatusCode.Accepted)
---
+++ BookingApi/Renditions.fs
@@ -0,0 +1,11 @@
+﻿namespace Ploeh.Samples.Booking.HttpApi
+
+open System
+
+[<CLIMutable>]
+type MakeReservationRendition = {
+    Date : string
+    Name : string
+    Email : string
+    Quantity : int }
+
